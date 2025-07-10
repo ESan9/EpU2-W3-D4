de94a8fe0fc71e92b974a545eff865fa40fb1686
EmanSan9 <sanna950@gmail.com>
2025-07-10 15:47:10 +0200

    2esercizi

--- script/script.js
+++ script/script.js
@@ -2,7 +2,30 @@ const button = document.getElementById("btnLoad");
 const images = document.getElementsByClassName("bd-placeholder-img");
 
 button.addEventListener("click", () => {
-  fetch("https://api.pexels.com/v1/search?query=hamsters&per_page=10", {
+  fetch("https://api.pexels.com/v1/search?query=hamsters", {
+    headers: {
+      Authorization: "g4814mGADrvd5BcbkvnrE8llkbM6PNbWPXGAaxF13c4stE0XmagpA5Ea",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      Array.from(images).forEach((img, index) => {
+        if (data.photos[index]) {
+          img.src = data.photos[index].src.medium;
+          img.alt = data.photos[index].photographer;
+        }
+      });
+    })
+    .catch((error) => {
+      console.error("Errore nel caricamento:", error);
+    });
+});
+
+const button1 = document.getElementById("btnLoad2");
+const images1 = document.getElementsByClassName("bd-placeholder-img");
+
+button1.addEventListener("click", () => {
+  fetch("https://api.pexels.com/v1/search?query=tigers", {
     headers: {
       Authorization: "g4814mGADrvd5BcbkvnrE8llkbM6PNbWPXGAaxF13c4stE0XmagpA5Ea",
     },
